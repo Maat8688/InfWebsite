8d087855c97380e87b587b5878e807aedf9768cc
Maat8688 <96978652+Maat8688@users.noreply.github.com>
2024-04-03 13:15:32 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
-nog maken
+#!/bin/bash
+
+set -e
+
+echo "Creating virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+
+echo "Build complete. Your Flask app is ready to run."
